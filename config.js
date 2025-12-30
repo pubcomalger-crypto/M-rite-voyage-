eab23c2da0fc4402afef2fdaee3b00d797e863c8
pubcomalger-crypto <pubcom.alger@gmail.com>
2025-12-30 14:33:56 +0100

    Mérite voyage
    Mérite voyage

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const profileConfig = {
     // Informations de base
 
 
-    name: "COCA",
-    bio: "SALAM",
-    profileImage: "https://i.ibb.co/NgKMhgMT/cocacola.jpg",
+    name: "Mérite voyage",
+    bio: "لأسفاركم ورحلاتكم الحل الأمثل",
+    profileImage: "https://scontent.falg6-2.fna.fbcdn.net/v/t39.30808-6/448524710_122154300824211169_3777626010354869069_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=hxWRVBoYIccQ7kNvwEn1_At&_nc_oc=Adm2QpcKwjzPOPxMBzMNCaahPe_zVNjHLoS9heGY8R9og1u9-uR0lkg2d0DeBAsuDo4&_nc_zt=23&_nc_ht=scontent.falg6-2.fna&_nc_gid=E9Jgj_ltgMEjbftRpXj5XA&oh=00_AfmdyvfS1-6HBvrEsweefDG4fqAnJhsjrRBMigTk19ij0Q&oe=69599D14",
     
 
 
@@ -15,17 +15,17 @@ const profileConfig = {
 
     // Réseaux sociaux
     socialLinks: {
-        facebook: "https://www.facebook.com/share/16dysjBQvt/?mibextid=wwXIfr",
-        instagram: "https://www.instagram.com/riyadmahrez26.7?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-        tiktok: "https://www.tiktok.com/@chebkhaled?is_from_webapp=1&sender_device=pc",
-        whatsapp: "+213540253369",
+        facebook: "https://www.facebook.com/share/1H9doLYku1/",
+        instagram: "https://www.instagram.com/merite_voyage?igsh=YWNjd3VpZm9wMXJ4",
+        tiktok: "https://www.tiktok.com/@merite.voyage?_r=1&_t=ZS-92RKGF6y21Y",
+        whatsapp: "+213550331341",
         telegram: "+213 562 33 14 08",
         linkedin: "daisyparkhouse",
         snapchat: "https://www.snapchat.com/@sheikh_tidiane?sender_web_id=792b2a4d-05d3-4e8f-a5e0-1aeb89eba722&device_type=desktop&is_copy_url=true",
-        email: "pubcom.alger@gmail.com",
+        email: "meritevoyage@gmail.com",
         website: "https://www.youtube.com/",
-        maps: "https://share.google/T1z7NAFbftnLYNKZy",
-        phone: "+213658546209"
+        maps: "https://goo.gl/maps/qxugj9RE4qxnDRpu7?g_st=aw",
+        phone: "+213550331341"
     },
     
 
@@ -40,12 +40,12 @@ const profileConfig = {
         instagram: true,     // Afficher Instagram
         tiktok: true,        // Afficher TikTok
         whatsapp: true,      // Afficher WhatsApp
-        telegram: true,      // Afficher Telegram
+        telegram: false,      // Afficher Telegram
         linkedin: false,     // Afficher LinkedIn (mettre true pour l'activer)
         website: false,       // Afficher Site Web
         maps: true,          // Afficher Maps
         phone: true,         // Afficher Téléphone
-        snapchat: true,      // Afficher Snapchat
+        snapchat: false,      // Afficher Snapchat
         email: true,         // Afficher Email
     },
     
@@ -83,3 +83,4 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.profileConfig = profileConfig;
 }
+
